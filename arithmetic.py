bda3effab731eddd046555763d130389b5bad8bc
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:54:08 -0800

    testing whether remote repo is connected

--- arithmetic.py
+++ arithmetic.py
@@ -8,16 +8,16 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return float(num1)/num2
+    return float(num1) / num2
 
 def square(num1):
     return num1 * num1
 
 def cube(num1):
-    return num1**3
+    return num1 ** 3
 
 def power(num1, num2):
-    return num1**num2
+    return num1 ** num2
 
 def mod(num1, num2):
-    return num1%num2
+    return num1 % num2
